884430e8ef6476ee93a800158bac3bbc04092a8a
Liuxiaogang <627614998@qq.com>
2016-07-24 13:35:04 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ src/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,54 @@
+package db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
+	/**
+	 * Province(省) 建表语句
+	 * 
+	 * */
+	public static final String CREATE_PROVINCE = "create table Province ("
+			+"id integer primary key autoincrement,"
+			+"province_name text,"
+			+"province_code text)";
+	
+	/**
+	 * City(市)
+	 * */
+	public static final String CREATE_CITY = "create table City("
+			+ "id integer primary key autoincrement,"
+			+ "city_name text"
+			+ "city_code text"
+			+ "province_id integer)";
+	
+	/**
+	 * county(县)
+	 * */
+	public static final String CREATE_COUNTY = "create table county("
+			+ "id integer primary key autoincrement,"
+			+ "county_name text"
+			+ "county_code text"
+			+ "city_id integer";
+	public CoolWeatherOpenHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		//  创建表
+		db.execSQL(CREATE_PROVINCE);
+		db.execSQL(CREATE_CITY);
+		db.execSQL(CREATE_COUNTY);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
